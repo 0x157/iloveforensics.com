7cd12b7e8e3df6950c06a54a9572856af3f0e4ef
i like kibana <102762345+0x157@users.noreply.github.com>
2024-06-13 02:50:29 -0400

    please work ffs

---
+++ _astro/translation.-Ktx0-r-.js
@@ -0,0 +1,1 @@
+import{A as n,B as o,C as e,D as t,E as r}from"./zh_TW.CQWaxzNX.js";const i=n,_={en:n,en_us:n,en_gb:n,en_au:n,zh_cn:o,zh_tw:e,ja:t,ja_jp:t};function u(n){return _[n.toLowerCase()]||i}function f(n){return u(r.lang)[n]}export{f as i};
